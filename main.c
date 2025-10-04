de5dcaac622761a7359b293d7d2da2465e141c7d
speckitor <pasaloginov381@gmail.com>
2025-10-04 21:50:57 +0700

    mods update

--- main.c
+++ main.c
@@ -9,7 +9,21 @@
 
 int clock_mtx[5][51] = {0};
 
-void setnumber(int number, int start) {
+static void print_help() {
+    printf("tinyclock - small terminal clock\n\n"
+           "Options:\n"
+           "\t-h, --help         print this message\n"
+           "\t-s, --stopwatch    use stopwatch mode, it will count from 0 until specified time\n"
+           "\t-t, --timer        use timer mode, it will count from specified time to 0\n\n"
+           "Time input:\n"
+           "\tin timer and stopwatch modes you can set time in the next format:\n"
+           "\th {time in hours} m {time in minutes} s {time in seconds}\n"
+           "\texample: tinyclock -t m 1\n"
+           "\tthis command will start timer that counts from 1 minute to 0\n"
+    );
+}
+
+static void setnumber(int number, int start) {
     for (int i = 0; i < 5; i++) {
         for (int j = start; j < start + 6; j++) {
             clock_mtx[i][j] = numbers[number][i][j - start];
@@ -17,7 +31,7 @@ void setnumber(int number, int start) {
     }
 }
 
-void settime(int hours, int minutes, int seconds) {
+static void settime(int hours, int minutes, int seconds) {
     setnumber(hours/10, 0);
     setnumber(hours%10, 7);
 
@@ -28,7 +42,7 @@ void settime(int hours, int minutes, int seconds) {
     setnumber(seconds%10, 45);
 }
 
-void print_clock() {
+static void print_clock() {
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
 
@@ -62,8 +76,8 @@ void print_clock() {
     }
 }
 
-void loop_forward(int hours, int minutes, int seconds) {
-    while (1) {
+static void clock_mode(int hours, int minutes, int seconds) {
+    while (true) {
         if (seconds > 59) {
             minutes++;
             seconds = 0;
@@ -87,8 +101,8 @@ void loop_forward(int hours, int minutes, int seconds) {
     }
 }
 
-void loop_back(int hours, int minutes, int seconds) {
-    while (1) {
+static void timer_mode(int hours, int minutes, int seconds) {
+    while (true) {
         if (seconds < 0) {
             minutes--;
             seconds = 59;
@@ -111,6 +125,34 @@ void loop_back(int hours, int minutes, int seconds) {
         seconds--;
     }
 }
+
+static void stopwatch_mode(int h, int m, int s) {
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+    while (hours != h || minutes != m || seconds != s || h + m + s == 0) {
+        if (seconds > 59) {
+            minutes++;
+            seconds = 0;
+        }
+
+        if (minutes > 59) {
+            hours++;
+            minutes = 0;
+        }
+
+        if (hours > 24) {
+            break;
+        }
+
+        settime(hours, minutes, seconds);
+
+        print_clock();
+
+        sleep(1);
+        seconds++;
+    }
+}
 int main(int argc, char **argv) {
     int hours = 0;
     int minutes = 0;
@@ -130,7 +172,10 @@ int main(int argc, char **argv) {
     clock_mtx[3][35] = 1;
 
     for (int i = 1; i < argc; ++i) {
-        if ((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--stopwatch") == 0)) {
+        if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0)) {
+            print_help();
+            return 0;
+        } else if ((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--stopwatch") == 0)) {
             stopwatch = true;
         } else if ((strcmp(argv[i], "-t") == 0) || (strcmp(argv[i], "--timer") == 0)) {
             timer = true;
@@ -158,6 +203,9 @@ int main(int argc, char **argv) {
             } else if (seconds < 0) {
                 seconds = 0;
             }
+        } else {
+            print_help();
+            return 0;
         }
     }
 
@@ -167,9 +215,9 @@ int main(int argc, char **argv) {
     }
 
     if (stopwatch) {
-        loop_forward(hours, minutes, seconds);
+        stopwatch_mode(hours, minutes, seconds);
     } else if (timer) {
-        loop_back(hours, minutes, seconds);
+        timer_mode(hours, minutes, seconds);
     } else {
         time_t rawtime;
         struct tm *timeinfo;
@@ -181,7 +229,7 @@ int main(int argc, char **argv) {
         minutes = timeinfo->tm_min;
         seconds = timeinfo->tm_sec;
 
-        loop_forward(hours, minutes, seconds);
+        clock_mode(hours, minutes, seconds);
     }
 
     return 0;
